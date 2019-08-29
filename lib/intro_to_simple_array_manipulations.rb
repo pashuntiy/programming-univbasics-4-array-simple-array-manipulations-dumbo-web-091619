7343b0e86574808a5593afb6c913c8229f9b2712
Pavel Ilin <pavel_ilin@yahoo.com>
2019-08-29 19:21:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,8 +42,8 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, 1)
-  array.delete(1)
+def using_delete_at(array, integer)
+  array.delete(integer)
 end
   
   
